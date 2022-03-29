6ba76d953ba4704731410d8f13489897698bf879
hlowell <91516214+sergeyeshakin@users.noreply.github.com>
2022-03-29 11:39:36 +0700

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -4,8 +4,8 @@
 #include "model/credit_model.h"
 
 
-TEST(s21_ariphmetic_test, 1) {
-    s21::Model mymodel("5.12 / 5 + 5.2 * 5 - -5.5");
+TEST(ariphmetic_test, 1) {
+    SmartCalc::Model mymodel("5.12 / 5 + 5.2 * 5 - -5.5");
     std::string realres("32.524");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -13,8 +13,8 @@ TEST(s21_ariphmetic_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_sincos_test, 1) {
-    s21::Model mymodel("sin(5) + cos(3)");
+TEST(sincos_test, 1) {
+    SmartCalc::Model mymodel("sin(5) + cos(3)");
     std::string realres("-1.948917");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -22,8 +22,8 @@ TEST(s21_sincos_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_tanatan_test, 1) {
-    s21::Model mymodel("tan(5) - atan(3)");
+TEST(my_tanatan_test, 1) {
+    SmartCalc::Model mymodel("tan(5) - atan(3)");
     std::string realres("-4.629561");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -31,8 +31,8 @@ TEST(s21_tanatan_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_acos_test, 1) {
-    s21::Model mymodel("acos(3)");
+TEST(acos_test, 1) {
+    SmartCalc::Model mymodel("acos(3)");
     std::string realres("nan");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -40,8 +40,8 @@ TEST(s21_acos_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_asin_test, 1) {
-    s21::Model mymodel("asin(1/2)");
+TEST(my_asin_test, 1) {
+    SmartCalc::Model mymodel("asin(1/2)");
     std::string realres("0.523599");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -49,8 +49,8 @@ TEST(s21_asin_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_lnlog_test, 1) {
-    s21::Model mymodel("ln(2) * log(3)");
+TEST(lnlog_test, 1) {
+    SmartCalc::Model mymodel("ln(2) * log(3)");
     std::string realres("0.330715");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -58,8 +58,8 @@ TEST(s21_lnlog_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_sqrtpow_test, 1) {
-    s21::Model mymodel("sqrt(81) + 3 ^ 3");
+TEST(sqrtpow_test, 1) {
+    SmartCalc::Model mymodel("sqrt(81) + 3 ^ 3");
     std::string realres("36");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -67,8 +67,8 @@ TEST(s21_sqrtpow_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_mod_test, 1) {
-    s21::Model mymodel("101.66 mod 10");
+TEST(mod_test, 1) {
+    SmartCalc::Model mymodel("101.66 mod 10");
     std::string realres("1.66");
     mymodel.polish_notation();
     mymodel.calculate_to_string();
@@ -76,8 +76,8 @@ TEST(s21_mod_test, 1) {
     ASSERT_EQ(realres, res);
 }
 
-TEST(s21_add_test, 2) {
-    s21::Model mymodel("50 + 50 + 30");
+TEST(add_test, 2) {
+    SmartCalc::Model mymodel("50 + 50 + 30");
     std::string realres("130");
     mymodel.polish_notation();
     mymodel.calculate_to_string(5);
@@ -86,7 +86,7 @@ TEST(s21_add_test, 2) {
 }
 
 TEST(credit_test, annuity) {
-    s21::CreditModel mymodel;
+    SmartCalc::CreditModel mymodel;
     std::vector<double> res = mymodel.annuity_payment(1000, 12, 12);
     std::vector<double> realres{1066.20, 66.20, 88.85};
     for (int i = 0; i < 3; i++)
@@ -94,7 +94,7 @@ TEST(credit_test, annuity) {
 }
 
 TEST(credit_test, dif) {
-    s21::CreditModel mymodel;
+    SmartCalc::CreditModel mymodel;
     std::vector<double> res = mymodel.dif_payment(10000, 6, 6);
     std::vector<double> realres{10175.00, 175.00, 1716.67, 1675.00};
     for (int i = 0; i < 4; i++)
@@ -102,7 +102,7 @@ TEST(credit_test, dif) {
 }
 
 TEST(graph_test, 1) {
-    s21::Model mymodel("x * x");
+    SmartCalc::Model mymodel("x * x");
     mymodel.polish_notation();
     std::vector<double> x, y;
     double plot_size[]{-10, 10, -10, 10};
