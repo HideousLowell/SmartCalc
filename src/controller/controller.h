dbcc5770f0f8fd90bbc869ec878fe5b6e29997de
hlowell <91516214+sergeyeshakin@users.noreply.github.com>
2022-03-29 11:38:10 +0700

    Update controller.h

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -5,7 +5,7 @@
 #include "../model/credit_model.h"
 #include "../model/model.h"
 
-namespace s21 {
+namespace SmartCalc {
 
 class Controller {
  private:
@@ -22,4 +22,4 @@ class Controller {
     std::vector<double> dif(double amount, int term, double rate);
 };
 
-}  // namespace s21
+}  // namespace SmartCalc
